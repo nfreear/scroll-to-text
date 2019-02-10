910559aac25fcbd78a092f1fa1ff6db6d759aec5
Nick Freear <nfreear@yahoo.co.uk>
2019-02-10 12:08:10 +0000

    Implement URL `param()` function; big changes [ci skip]

--- src/scroll-to-text.js
+++ src/scroll-to-text.js
@@ -5,10 +5,14 @@
 */
 
 const highlight = require('./highlight-phrase').highlight;
-const L = window.location;
+const fetch = window.fetch;
+const LOC = window.location;
 
 const DEFAULTS = {
   selector: '#scroll-to-text',
+  url: param('url', 'https://americanrhetoric.com/speeches/mlkihaveadream.htm'),
+  text: param('text', 'its creed'),
+  background: param('bg', 'yellow'),
   // corsProxy: 'https://corsproxy.github.io/?',
   corsProxy: 'https://cors-anywhere.herokuapp.com/',
   example: {
@@ -16,52 +20,60 @@ const DEFAULTS = {
     text: 'May I offer you a glass'
     // text: 'EPUB (no images)' // 'We have been allies'
   },
+  pagePrefix: [
+    '<base href="{u}" target="_top" />'
+  ],
   pageSuffix: [
     '\n<!-- Injected by: scroll-to-text.js -->',
-    '<base href="{u}" target="_top" />',
+    // '<base href="{u}" target="_top" />',
     '<script src="{b}/src/scroll-to-driver.js"></script>',
     '<style> .wh-highlight { background: {c}; border-radius: 5px; padding: 4px; }</style>',
     ''
   ],
-  background: 'yellow',
-  baseUrl: L.origin
+  baseUrl: LOC.origin
 };
 
 const CFG = /* extends */ DEFAULTS;
-const QUERY = window.location.search;
-const fetch = window.fetch;
 
-var urlMatch = QUERY.match(/url=(https?.+?)(:?&|$)/);
-var textMatch = QUERY.match(/text=(.+?)(:?&|$)/);
-var colorMatch = QUERY.match(/bg=(\w+?)(:?&|$)/);
+const HOLDER = qs(CFG.selector);
+const IFRAME = qs('iframe', HOLDER);
 
-const URL = decodeURIComponent(urlMatch ? urlMatch[ 1 ] : CFG.example.url);
-const TEXT = decodeURIComponent(textMatch ? textMatch[ 1 ] : CFG.example.text);
-const COLOR = decodeURIComponent(colorMatch ? colorMatch[ 1 ] : CFG.background);
+const REQUEST_URL = CFG.corsProxy + CFG.url;
 
-const HOLDER = q(CFG.selector);
-const IFRAME = q('iframe', HOLDER);
-
-const REQUEST_URL = CFG.corsProxy + URL;
-
-console.warn('[STT] text:', TEXT, CFG);
+console.warn('[STT] config:', CFG);
 
 fetch(REQUEST_URL)
   .then(resp => resp.text())
   .then(html => {
     console.warn('[STT] then page:', html);
 
-    const SUFFIX = CFG.pageSuffix.join('\n').replace('{u}', URL).replace('{b}', CFG.baseUrl).replace('{c}', COLOR);
-    const NEW_HTML = highlight(html, TEXT) + SUFFIX;
+    const SUFFIX = CFG.pageSuffix.join('\n').replace('{u}', CFG.url).replace('{b}', CFG.baseUrl).replace('{c}', CFG.background);
+    const PREFIX = CFG.pagePrefix.join('\n').replace('{u}', CFG.url);
+
+    const NEW_HTML = PREFIX + highlight(html, CFG.text) + SUFFIX;
 
     // Fix only needed for 'file:' protocol.
     const FIX_HTML = NEW_HTML; // .replace(/src="\/\//g, 'src="https://');
 
     console.warn('[STT] modified page:', FIX_HTML);
 
-    IFRAME.setAttribute('srcdoc', /* BASE + */ NEW_HTML); /* 'data:text/html,' */
-  });
+    IFRAME.setAttribute('srcdoc', NEW_HTML); /* 'data:text/html,' */
+  })
+  .catch(error => console.error(error));
 
-function q (selector, element) {
+function qs (selector, element) {
   return (element || document).querySelector(selector);
+}
+
+function param (pmName, defaultPm) {
+  defaultPm = defaultPm || null;
+  const RE = new RegExp('[?&]' + pmName + '=(.+?)(:?&|$)'); // (/text=(.+?)(:?&|$)/) (/url=(https?.+?)(:?&|$)/) (/bg=(\w+?)(:?&|$)/);
+  const MAT = LOC.search.match(RE);
+  const VALUE = decodeURIComponent(MAT ? MAT[ 1 ] : defaultPm);
+
+  // Security.
+  if (/([<>;]|javascript:)/i.test(VALUE)) {
+    throw Error('[STT] Security: XSS injection attempt :~ ' + VALUE);
+  }
+  return VALUE;
 }
